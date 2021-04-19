02f183e24a4b205f121c1b5ed1bd53a717a6d34d
BuildTools <unconfigured@null.spigotmc.org>
2021-04-19 12:34:54 +0100

    Add a random number generator

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, TextInput, View, Button, SafeAreaView, Alert, } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button, SafeAreaView, Alert, Keyboard } from 'react-native';
 
 export default function App() {
+  const [randomValue, setRandomValue] = useState(Math.floor(Math.random() * 12) + 1);
   return (
     <View style={styles.container}>
-      <Text>Question would go here</Text>
-      <TextInput style={styles.inputBox}></TextInput>
-      <Button 
-      title="Enter"
-      onPress={() => Alert.alert('Button pressed')}
-      />
+      <Text>{randomValue}</Text>
+
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.inputBox}></TextInput>
+        <Button
+        title="Enter" 
+        onPress={() => setRandomValue(Math.floor(Math.random() * 12) + 1)}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -27,5 +31,9 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 5,
     borderStyle: 'dashed',
-  }
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
 });
