75ad5db722cd41b520a4d106ca775d0d01db63be
BuildTools <unconfigured@null.spigotmc.org>
2021-04-20 10:06:50 +0100

    Add checks for impossible subtractions

--- App.js
+++ App.js
@@ -1,23 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import { StyleSheet, Text, TextInput, View, Button, SafeAreaView, Alert, Keyboard } from 'react-native';
 
 export default function App() {
   const [randomValue, setRandomValue] = useState(Math.floor(Math.random() * 12) + 1);
+  const [randomValue2, setRandomValue2] = useState(Math.floor(Math.random() * 12) + 1);
+  const [randomSymbol, setRandomSymbol] = useState(Math.floor(Math.random() * 3));
+  const symbolTypes = ["+","-","*","/"]
+
+  function newNumbers() {
+    setRandomValue(Math.floor(Math.random() * 12) + 1);
+    setRandomValue2(Math.floor(Math.random() * 12) + 1);
+    setRandomSymbol(Math.floor(Math.random() * 4));
+
+  }
+
+  if (randomSymbol == 1 || randomSymbol == 3) {
+    if (randomValue2 > randomValue) {
+      newNumbers()
+    }
+    else if (randomSymbol == 3 && randomValue2 % randomValue != 0) {
+      newNumbers()
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>{randomValue}</Text>
+      <Text>{randomValue} {symbolTypes[randomSymbol]} {randomValue2}</Text>
 
       <View style={styles.inputContainer}>
         <TextInput style={styles.inputBox}></TextInput>
         <Button
         title="Enter" 
-        onPress={() => setRandomValue(Math.floor(Math.random() * 12) + 1)}
+        onPress={newNumbers}
         />
       </View>
       <StatusBar style="auto" />
     </View>
   );
+
+
+    
+
 }
 
 const styles = StyleSheet.create({
