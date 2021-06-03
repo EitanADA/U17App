bff3838ab44ae3b57564cc95df1511c7ffbacefd
EitanADA <eitan.abelman@ada.ac.uk>
2021-06-03 16:25:00 +0100

    Added optimisations, timer not working

--- App.js
+++ App.js
@@ -5,10 +5,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AppLoading from 'expo-app-loading';
 import { Audio } from 'expo-av';
+//import the component that loads sounds from an external file
 import SoundComponent from './sound';
+//the sound files that aren't played async need to be loaded as components from the local files
 import wrongSound from './assets/wrong.mp3';
 import correctSound from './assets/button.mp3';
 
+//this uses fonts from google fonts, ensure they work cross platform and they are web friendly
 import {
   useFonts,
   FredokaOne_400Regular 
@@ -17,11 +20,13 @@ import {
   Nunito_400Regular,
 } from '@expo-google-fonts/nunito'
 
+//this is the first screen of the 
 function HomeScreen({ navigation }) {
 
   const [sound, setSound] = React.useState();
-  const [sound2, setSound2] = React.useState();
+  // const [sound2, setSound2] = React.useState();
 
+  //this will load the sound and play it when it has loaded
   async function playSound() {
     const { sound } = await Audio.Sound.createAsync(
        require('./assets/whoosh.mp3')
@@ -32,10 +37,12 @@ function HomeScreen({ navigation }) {
   React.useEffect(() => {
     return sound
       ? () => {
+        //unload the sound when it is finished playing
           sound.unloadAsync(); }
       : undefined;
   }, [sound]);
 
+  //this function will make the font a size that is reponsive to the size of the screen
   function calcSize(size) {
     if (Platform.OS === 'ios') {
       return Math.round(PixelRatio.roundToNearestPixel(size * Dimensions.get('window').width / 320))
@@ -44,16 +51,20 @@ function HomeScreen({ navigation }) {
     }
   }
 
+  //this will load the fonts from google fonts, using the package expo-fonts
   let [fontsLoaded] = useFonts({
     FredokaOne_400Regular,
     Nunito_400Regular,});
   
+    //if the fonts haven't loaded yet, the aoo should show a loading screen
     if (!fontsLoaded) {
       return <AppLoading />;
     } else {
+      //when the fonts have loaded in, the rest of the document will be rendered
       return (
       <View style={{flex: 1, backgroundColor: "#75DDDD", alignItems: 'center', justifyContent: 'center'}}>
         <Text style={{fontFamily: "FredokaOne_400Regular", color: "white", fontSize: calcSize(40)}}>Maths Master</Text>
+        {/* buttons cannot be styled so you have to make your own by using the TouchableOpacity component */}
         <TouchableOpacity>
         <Text style={{fontFamily: "Nunito_400Regular", fontSize: calcSize(35),paddingHorizontal: 20, paddingVertical: 10, borderRadius: 20, elevation: 4, textAlign: 'center', backgroundColor: "#ffaa5d",}}
         onPress={() => {
@@ -64,12 +75,15 @@ function HomeScreen({ navigation }) {
         </Text>
         </TouchableOpacity>
         <View>
+          {/* there is no conventional way to do a line break so this is used instead */}
         <Text>{"\n"}</Text>
       </View>
         <TouchableOpacity>
         <Text style={{fontFamily: "Nunito_400Regular", fontSize: calcSize(25),paddingHorizontal: 20, paddingVertical: 10, borderRadius: 20, elevation: 4, textAlign: 'center', backgroundColor: "#ffaa5d",}}
         onPress={() => {
+          //this will call the playSound function declared previously
           playSound()
+          //this willmove to the tutorial screen
           navigation.navigate('Tutorial')
           }}>
         How to Play
@@ -81,7 +95,23 @@ function HomeScreen({ navigation }) {
 };
 
 function Tutorial({ navigation }) {
+  //this is repeated from the previous screen
+  const [sound, setSound] = React.useState();
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync(
+       require('./assets/select.mp3')
+    );
+    setSound(sound);
+    await sound.playAsync(); }
 
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          sound.unloadAsync(); }
+      : undefined;
+  }, [sound]);
+
+  //repeated from previous screen
   function calcSize(size) {
     if (Platform.OS === 'ios') {
       return Math.round(PixelRatio.roundToNearestPixel(size * Dimensions.get('window').width / 320))
@@ -98,14 +128,24 @@ function Tutorial({ navigation }) {
       return <AppLoading />;
     } else {
       return (
-      <View style={{flex: 1, backgroundColor: "#75DDDD", alignItems: 'center', justifyContent: 'center'}}>
-        <Text style={{fontFamily: "FredokaOne_400Regular", color: "white", fontSize: calcSize(40)}}>Maths Master</Text>
+      <View style={{flex: 1, backgroundColor: "#0099cc", alignItems: 'center', justifyContent: 'center',}}>
+        <Text style={{fontFamily: "Nunito_400Regular", color: "white", fontSize: calcSize(30), marginRight: 30, marginLeft: 30,}}>How to Play:</Text>
+        <Text>{"\n"}</Text>
+        <Text style={{fontFamily: "Nunito_400Regular", color: "white", fontSize: calcSize(15), marginRight: 30, marginLeft: 30,}}>You will be asked 10 arithmetic questions.</Text>
+        <Text>{"\n"}</Text>
+        <Text style={{fontFamily: "Nunito_400Regular", color: "white", fontSize: calcSize(15), marginRight: 30, marginLeft: 30,}}>Write the answer in the box below the question, and press the "Enter" button next to the box.</Text>
+        <Text>{"\n"}</Text>
+        <Text style={{fontFamily: "Nunito_400Regular", color: "white", fontSize: calcSize(12), marginRight: 30, marginLeft: 30,}}>If you choose "Medium" or "Hard" levels, then there will be a timer above the question. Try to answer before it runs out</Text>
+        <Text>{"\n"}</Text>
+        <Text style={{fontFamily: "Nunito_400Regular", color: "white", fontSize: calcSize(17), marginRight: 30, marginLeft: 30,}}>Good luck and have fun!</Text>
+        <Text>{"\n"}</Text>
         <TouchableOpacity>
-        <Text style={{fontFamily: "Nunito_400Regular", fontSize: calcSize(35),paddingHorizontal: 20, paddingVertical: 10, borderRadius: 20, elevation: 4, textAlign: 'center', backgroundColor: "#ffaa5d",}}
+        <Text style={{fontFamily: "Nunito_400Regular", fontSize: calcSize(35),paddingHorizontal: 20, paddingVertical: 10, borderRadius: 20, elevation: 4, textAlign: 'center', backgroundColor: "#cc3300",}}
         onPress={() => {
           playSound()
           navigation.navigate('Levels')
           }}>
+            {/* this will navigate to the next level selection screen */}
         Play
         </Text>
         </TouchableOpacity>
@@ -124,7 +164,6 @@ function LevelPage({ navigation }) {
   }
   
   const [sound, setSound] = React.useState();
-  const [sound2, setSound2] = React.useState();
 
   async function playSound() {
     const { sound } = await Audio.Sound.createAsync(
@@ -140,12 +179,11 @@ function LevelPage({ navigation }) {
       : undefined;
   }, [sound]);
 
-
   return (
 
     <View style={{alignItems: 'center', justifyContent: 'center', backgroundColor: "#FF729F", flex: 1,}}>      
       <View style={{paddingHorizontal: 20, paddingVertical: 20, borderRadius: 20, elevation: 4, textAlign: 'center', backgroundColor: "#9368B7",}}>
-        <Text style={{fontFamily: "Nunito_400Regular", fontSize: calcSize(35),}}>
+        <Text style={{fontFamily: "Nunito_400Regular", fontSize: calcSize(35), color: 'white'}}>
           Choose a Level:
         </Text>
       </View>
@@ -156,12 +194,13 @@ function LevelPage({ navigation }) {
         <TouchableOpacity
         onPress={() => {
           playSound()
+          // easy mode will use -1 to logically declare that eh timer shouldn't be used
           navigation.navigate('Questions', {timer: -1,})
         }}>
-          <Text style={{fontFamily: "Nunito_400Regular", fontSize: calcSize(30),}}>
+          <Text style={{fontFamily: "Nunito_400Regular", fontSize: calcSize(30), color: 'white'}}>
             Easy
           </Text>
-          <Text style={{fontFamily: "Nunito_400Regular", fontSize: calcSize(20),}}>No timer</Text>
+          <Text style={{fontFamily: "Nunito_400Regular", fontSize: calcSize(20), color: 'white'}}>No timer</Text>
         </TouchableOpacity>
       </View>
       <View>
@@ -172,9 +211,9 @@ function LevelPage({ navigation }) {
         onPress={() =>
           navigation.navigate('Questions', {timer: 20,})
         }>
-        <Text style={{fontFamily: "Nunito_400Regular", fontSize: calcSize(30),}}>
+        <Text style={{fontFamily: "Nunito_400Regular", fontSize: calcSize(30), color: 'white'}}>
         Medium</Text>
-        <Text style={{fontFamily: "Nunito_400Regular", fontSize: calcSize(20),}}>20 second timer</Text>
+        <Text style={{fontFamily: "Nunito_400Regular", fontSize: calcSize(20), color: 'white'}}>20 second timer</Text>
         </TouchableOpacity>
       </View>
       <View>
@@ -185,9 +224,9 @@ function LevelPage({ navigation }) {
         onPress={() =>
           navigation.navigate('Questions', {timer: 10,})
         }>
-        <Text style={{fontFamily: "Nunito_400Regular", fontSize: calcSize(30),}}>
+        <Text style={{fontFamily: "Nunito_400Regular", fontSize: calcSize(30), color: 'white'}}>
         Hard</Text>
-        <Text style={{fontFamily: "Nunito_400Regular", fontSize: calcSize(20),}}>10 second timer</Text>
+        <Text style={{fontFamily: "Nunito_400Regular", fontSize: calcSize(20), color: 'white'}}>10 second timer</Text>
       </TouchableOpacity>
     </View>
     </View>
@@ -195,6 +234,7 @@ function LevelPage({ navigation }) {
 }
 
 function QuestionPage({ route, navigation }) {
+  // routes are used to accept the selected difficulty from the previous screen, by using the amount of time on the time
   const { timer } = route.params;
   const [text, setText] = useState('');
   const [question, setQuestion] = useState({
@@ -256,7 +296,6 @@ function QuestionPage({ route, navigation }) {
   })
 
   function checkAnswer() {
-    //setTimeout(function(){setFeedback('')}, 1000);
     
     newNumbers();
     if (questionCount < 10) {
@@ -264,8 +303,8 @@ function QuestionPage({ route, navigation }) {
     }
 
     if (timer > 0) {
-      setDisplayTimer(timer);
       clearTimeout(t);
+      setDisplayTimer(timer);
     }
     if (userAnswer == solution && userAnswer != null){
       setFeedback('correct 🎉');
@@ -327,7 +366,7 @@ function QuestionPage({ route, navigation }) {
       defaultValue={text}
       autoFocus={true}
       keyboardType={'numeric'}
-      style={{fontFamily: "Nunito_400Regular", fontSize: calcSize(20), borderWidth: 2, borderRadius: 5, borderStyle: 'dashed', textAlign: 'center',}}>
+      style={{fontFamily: "Nunito_400Regular", fontSize: calcSize(14), borderWidth: 2, borderRadius: 5, borderStyle: 'dashed', textAlign: 'center', marginRight: 30, marginLeft: 20,}}>
       </TextInput>
 
       <TouchableOpacity
